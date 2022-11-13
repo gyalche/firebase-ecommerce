ad1eeb22c398d98ed6d74465c79b110134e8d050
Dawa <sherpadawa01@outlook.com>
2022-11-13 18:51:36 +0545

    loader created

---
+++ src/components/loader/loader.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './Loader.module.scss';
+import loading from '../../assets/loader.gif';
+import ReactDOM from 'react-dom';
+
+const loader = () => {
+  return ReactDOM.createPortal(
+    <div className={styles.wrapper}>
+      <div className={styles.loader}>
+        <img src={loading} alt="loading" />
+      </div>
+    </div>,
+    document.getElementById('loader')
+  );
+};
+
+export default loader;
